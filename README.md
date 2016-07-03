6d026fbbc51533a42fc18a48464f7f0ba9c2fc0d
otanim <otanim617@gmail.com>
2016-07-03 04:28:08 +0400

    release

---
+++ README.md
@@ -0,0 +1,39 @@
+# Armenian transliteration library.
+
+## Install
+
+```sh
+$ npm install https://github.com/LinguaCode/linguacode-logger --save
+```
+
+
+## Usage
+
+```javascript
+var logger = require('linguacode-logger');
+
+logger(1);
+logger(2);
+logger(3);
+logger(4);
+logger(3);
+logger(2);
+logger(1);
+```
+
+```
+1
+    2
+        3
+            4
+        3
+    2
+1
+1
+    2
+1
+```
+
+## License
+
+MIT © [Otanim](http://otanim.com)
---
+++ index.js
@@ -0,0 +1,111 @@
+let _ = require('lodash');
+let colors = require('colors');
+
+let env = process.env.NODE_ENV;
+let colorReservation = [];
+
+let isScopeOpened = function (command) {
+    let countOfElements = 0;
+
+    for (let i = 0; i < colorReservation.length; i++) {
+        if (colorReservation[i].command == command) {
+            countOfElements++;
+        }
+    }
+
+    return countOfElements % 2 == 1;
+};
+
+let indentGenerate = function () {
+    let space = ' ';
+    let spaces = '';
+
+    for (let i = 0; i < 4; i++) {
+        spaces += space;
+    }
+
+    return spaces;
+};
+let indentsGenerate = function (levels) {
+    let indent = indentGenerate();
+    let indents = '';
+    for (let i = 0; i < levels; i++) {
+        indents += indent;
+    }
+
+    return indents;
+};
+
+let colorParamsGenerate = function (index) {
+    const colorList = [
+        'white',
+        'green',
+        'yellow',
+        'blue',
+        'magenta',
+        'cyan',
+        'red'
+    ];
+
+    let countOfColors = colorList.length;
+
+
+    let indexOfColor =
+      index < countOfColors ?
+        index :
+      index % countOfColors;
+
+    return colorList[indexOfColor]
+};
+let oldSibling = function (command) {
+    let listOfReservedTexts = _.map(colorReservation, 'command');
+    let indexOfLastResult = listOfReservedTexts.lastIndexOf(command);
+
+    if (indexOfLastResult != -1) {
+        return colorReservation[indexOfLastResult];
+    }
+
+    return false;
+};
+
+let getLastIndentIndex = function (colorReservationList, command) {
+    let countOfReservedColors = colorReservation.length;
+    if (countOfReservedColors == 0) {
+        return 0;
+    }
+
+    let sibling = oldSibling(command);
+    if (sibling) {
+        return sibling.indent;
+    }
+
+    return colorReservationList[countOfReservedColors - 1].indent + 1;
+};
+
+module.exports = function (command) {
+    if (env != 'testing') {
+        return;
+    }
+
+    let color;
+    let indents;
+    if (!isScopeOpened(command)) {
+        let countOfReservedColors = colorReservation.length;
+        color = colorParamsGenerate(countOfReservedColors);
+    } else {
+        let sibling = oldSibling(command);
+        color = sibling.colors;
+    }
+
+    let indentParam = getLastIndentIndex(colorReservation, command);
+    indents = indentsGenerate(indentParam);
+    colorReservation.push({
+        command: command,
+        colors: color,
+        indent: indentParam
+    });
+
+    let text = indents + colors[color](command);
+
+    console.log(text);
+};
