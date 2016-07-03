e96cbd722a1f4c93eb9efc74b46ced1ad0d46415
otanim <otanim617@gmail.com>
2016-07-03 17:26:17 +0400

    finalize

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-let _ = require('lodash');
 let colors = require('colors');
 
 let colorReservation = [];
 
-let isScopeOpened = function (command) {
-  let countOfElements = 0;
+let generate = {
+  indent: function () {
+    let space = ' ';
+    let spaces = '';
 
-  for (let i = 0; i < colorReservation.length; i++) {
-    if (colorReservation[i].command == command) {
-      countOfElements++;
+    for (let i = 0; i < 4; i++) {
+      spaces += space;
+    }
+
+    return spaces;
+  },
+  indents: function (levels) {
+    let indent = generate.indent();
+    let indents = '';
+    for (let i = 0; i < levels; i++) {
+      indents += indent;
+    }
+
+    return indents;
+  }
+};
+
+let isScopeOpened = function (command, indent) {
+  for (var i = colorReservation.length - 1; i >= 0; i--) {
+    if (colorReservation[i].command == command && colorReservation[i].destination == 'start' && colorReservation[i].indent == indent && colorReservation[i].isClosed != true) {
+      return true;
     }
   }
 
-  return countOfElements % 2 == 1;
-};
-
-let indentGenerate = function () {
-  let space = ' ';
-  let spaces = '';
-
-  for (let i = 0; i < 4; i++) {
-    spaces += space;
-  }
-
-  return spaces;
-};
-let indentsGenerate = function (levels) {
-  let indent = indentGenerate();
-  let indents = '';
-  for (let i = 0; i < levels; i++) {
-    indents += indent;
-  }
-
-  return indents;
+  return false;
 };
 
 let colorParamsGenerate = function (index) {
@@ -56,36 +55,36 @@ let colorParamsGenerate = function (index) {
 
   return colorList[indexOfColor]
 };
-let oldSibling = function (command) {
-  let listOfReservedTexts = _.map(colorReservation, 'command');
-  let indexOfLastResult = listOfReservedTexts.lastIndexOf(command);
-
-  if (indexOfLastResult != -1) {
-    return colorReservation[indexOfLastResult];
+let getIndexOfOldSibling = function (command) {
+  for (var i = colorReservation.length - 1; i >= 0; i--) {
+    if (colorReservation[i].command == command && colorReservation[i].destination == 'start' && !colorReservation[i].isClosed) {
+      return i;
+    }
   }
 
   return false;
 };
 
-let getLastIndentIndex = function (colorReservationList, command, isSingle = false) {
+let getLastIndentIndex = function (colorReservationList, command, destination) {
   let countOfReservedColors = colorReservation.length;
   if (countOfReservedColors == 0) {
     return 0;
   }
 
-  if (isSingle) {
-    return colorReservationList[countOfReservedColors - 1].indent;
+  if (destination == 'start') {
+    return colorReservationList[countOfReservedColors - 1].indent + 1;
+  } else if (destination == 'end') {
+    let indexOfOldSibling = getIndexOfOldSibling(command);
+    let sibling = colorReservation[indexOfOldSibling];
+    if (sibling) {
+      return sibling.indent;
+    }
   }
 
-  let sibling = oldSibling(command);
-  if (sibling) {
-    return sibling.indent;
-  }
-
-  return colorReservationList[countOfReservedColors - 1].indent + 1;
+  return colorReservationList[countOfReservedColors - 1].indent;
 };
 
-console.llog = module.exports = function (command, isSingle) {
+console.llog = module.exports = function (command, destination) {
   let env = process.env.NODE_ENV;
   if (env != 'testing') {
     return;
@@ -93,20 +92,25 @@ console.llog = module.exports = function (command, isSingle) {
 
   let color;
   let indents;
-  if (!isScopeOpened(command)) {
+  let indent = getLastIndentIndex(colorReservation, command, destination);
+  if (!isScopeOpened(command, indent)) {
     let countOfReservedColors = colorReservation.length;
     color = colorParamsGenerate(countOfReservedColors);
   } else {
-    let sibling = oldSibling(command);
+    let indexOfOldSibling = getIndexOfOldSibling(command);
+    colorReservation[indexOfOldSibling].isClosed = true;
+
+    let sibling = colorReservation[indexOfOldSibling];
+
     color = sibling.colors;
   }
 
-  let indentParam = getLastIndentIndex(colorReservation, command, isSingle);
-  indents = indentsGenerate(indentParam);
+  indents = generate.indents(indent);
   colorReservation.push({
     command: command,
     colors: color,
-    indent: indentParam
+    indent: indent,
+    destination: destination
   });
 
   let text = indents + colors[color](command);
