099aec78a23ccd798da4d032bb5c2cd46c88c82c
otanim <otanim617@gmail.com>
2016-07-17 04:14:41 +0400

    first publish; mephistopheles approved

--- README.md
+++ README.md
@@ -1,80 +1,27 @@
-# Logger for [linguacode-api](https://github.com/LinguaCode/linguacode-api).
+# node-cache-wiper
 
 ## Description
-**linguacode-logger** is a hyper recursive logging tool which helps to create simple charts for a complex hyper recursions.
-
-## Screenshot
-![Screenshot](https://raw.githubusercontent.com/LinguaCode/linguacode-logger/master/screenshots/image.png)
+**node-cache wiper** is a cache wiper of the required modules.
 
 ## How to install
 
 ```sh
-$ npm install https://github.com/LinguaCode/linguacode-logger --save
+$ npm install https://github.com/otanim/node-cache-wiper --save
 ```
 
 ### Usage
 
-##### require the **linguacode-logger** module.
 ```javascript
-var logger = require('linguacode-logger');
+var cacheWiper = require('node-cache-wiper');
+
+var serverPath = './src/server';
+
+var server = require(serverPath); //content of the "server" file now was cached
+
+cacheWiper(serverPath); //cache of the 'server" file now was wiped
+server = require(serverPath); //content of the "server" file now was AGAIN cached
 ```
 
-##### prints single `message`.
-```javascript
-logger.log(message)
-```
-
-##### same as `logger.log`.
-```javascript
-console.llog(message)
-```
-
-##### prints single collapse's first `message`.
-```javascript
-console.llog(message, 'begin')
-```
-
-##### prints single collapse's last `message`.
-```javascript
-console.llog(message, 'end')
-```
-
-##### reset the indent level.
-```javascript
-logger.init()
-```
-
-
-## Example
-
-### Code
-
-```javascript
-var logger = require('linguacode-logger');
-
-console.llog('single');
-console.llog('1st level', 'begin');
-console.llog('2nd level', 'begin');
-console.llog('3rd level', 'begin');
-console.llog('single');
-console.llog('3rd level', 'end');
-console.llog('2nd level', 'end');
-console.llog('1st level', 'end');
-console.llog('single');
-```
-
-### Output
-```
-single
-1st level
-    2nd level
-        3rd level
-            single
-        3rd level
-    2nd level
-1st level
-single
-```
 
 ##License
-LinguaCode is [licensed under GPLv3](https://github.com/LinguaCode/linguacode-logger/blob/master/LICENSE).
+node-cache-wiper is [licensed under MIT](https://github.com/otanim/node-cache-wiper/blob/master/LICENSE).
--- index.js
+++ index.js
@@ -1,139 +1,56 @@
-let colors = require('colors');
+var path = require('path');
 
-let colorReservation = [];
-
-let generate = {
-  indent: function () {
-    let space = ' ';
-    let spaces = '';
-
-    for (let i = 0; i < 4; i++) {
-      spaces += space;
-    }
-
-    return spaces;
-  },
-  indents: function (levels) {
-    let indent = generate.indent();
-    let indents = '';
-    for (let i = 0; i < levels; i++) {
-      indents += indent;
-    }
-
-    return indents;
-  },
-
-  color: function (index) {
-    const colorList = [
-      'white',
-      'green',
-      'yellow',
-      'gray',
-      'magenta',
-      'cyan',
-      'red'
-    ];
-
-    let countOfColors = colorList.length;
-
-
-    let indexOfColor =
-      index < countOfColors ?
-        index :
-      index % countOfColors;
-
-    return colorList[indexOfColor]
-  }
-};
-
-let isScopeOpened = function (command, indent) {
-  for (var i = colorReservation.length - 1; i >= 0; i--) {
-    if (colorReservation[i].command == command && colorReservation[i].destination == 'begin' && colorReservation[i].indent == indent && colorReservation[i].isClosed != true) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-let getIndexOfOldSibling = function (command) {
-  for (var i = colorReservation.length - 1; i >= 0; i--) {
-    if (colorReservation[i].command == command && colorReservation[i].destination == 'begin' && !colorReservation[i].isClosed) {
-      return i;
-    }
-  }
-
-  return false;
-};
-
-let getLastIndentIndex = function (colorReservationList, command, destination) {
-  let countOfReservedColors = colorReservation.length;
-  if (countOfReservedColors == 0) {
-    return 0;
-  }
-
-  if (destination == 'begin') {
-    for (var i = colorReservation.length - 1; i >= 0; i--) {
-      if (colorReservationList[i].destination && !colorReservationList[i].isClosed) {
-        break;
-      }
-    }
-    if (i != -1) {
-      return colorReservationList[i].indent + 1;
-    }
-
-    return colorReservationList[countOfReservedColors - 1].indent;
-  } else if (destination == 'end') {
-    let indexOfOldSibling = getIndexOfOldSibling(command);
-    let sibling = colorReservation[indexOfOldSibling];
-    if (sibling) {
-      return sibling.indent;
-    }
-  }
-
-  if (colorReservationList[countOfReservedColors - 1].destination == 'begin') {
-    return colorReservationList[countOfReservedColors - 1].indent + 1;
-  }
-
-  return colorReservationList[countOfReservedColors - 1].indent;
-};
-
-console.llog = exports.log = function (command, destination) {
-  let env = process.env.DEBUG;
-  if (env != 'true') {
-    return;
-  }
-
-  let color;
-  let indents;
-  let isClosed = false;
-  let indent = getLastIndentIndex(colorReservation, command, destination);
-  if (!isScopeOpened(command, indent)) {
-    let countOfReservedColors = colorReservation.length;
-    color = generate.color(countOfReservedColors);
-  } else {
-    let indexOfOldSibling = getIndexOfOldSibling(command);
-    colorReservation[indexOfOldSibling].isClosed = true;
-    isClosed = true;
-    let sibling = colorReservation[indexOfOldSibling];
-
-    color = sibling.colors;
-  }
-
-  indents = generate.indents(indent);
-  colorReservation.push({
-    command: command,
-    colors: color,
-    indent: indent,
-    destination: destination,
-    isClosed: isClosed
+/**
+ * Removes a module from the cache
+ */
+module.exports = function (moduleName) {
+  // Traverse the cache looking for the files
+  // loaded by the specified module name
+  searchCache(moduleName, function (mod) {
+    delete require.cache[mod.id];
   });
 
-  let text = indents + colors[color](command);
+  // Remove cached paths to the module.
+  // Thanks to @akhoury for pointing this out.
+  for (var cacheKey in module.constructor._pathCache) {
+    var moduleNameLength = moduleName.length;
 
-  console.log(text);
+    var cacheKeyParsed = JSON.parse(cacheKey);
+    var requestPathOfCacheKey = cacheKeyParsed.request;
+    var requestPathOfCacheKeyLength = requestPathOfCacheKey.length;
+
+    var indexOfModuleName = requestPathOfCacheKey.lastIndexOf(moduleName);
+    var correctIndexOfModuleName = requestPathOfCacheKeyLength - moduleNameLength;
+
+    if (indexOfModuleName !== -1 && indexOfModuleName == correctIndexOfModuleName) {
+      delete module.constructor._pathCache[cacheKey];
+      break;
+    }
+  }
 };
 
-exports.init = function () {
-  colorReservation = [];
-};
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache(moduleName, callback) {
+  // Resolve the module identified by the specified name
+  var mod = require.resolve(path.join(process.cwd(), moduleName));
+
+  // Check if the module has been resolved and found within
+  // the cache
+  if (mod && ((mod = require.cache[mod]) !== undefined)) {
+    // Recursively go over the results
+    (function traverse(mod) {
+      // Go over each of the module's children and
+      // traverse them
+      mod.children.forEach(function (child) {
+        traverse(child);
+      });
+
+      // Call the specified callback providing the
+      // found cached module
+      callback(mod);
+    }(mod));
+  }
+}
